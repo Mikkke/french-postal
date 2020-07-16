1122420a0f76c4e7d9762d456da9595869926a73
Mikkke <jkellymichael@gmail.com>
2020-07-16 16:51:03 +0200

    Updates

---
+++ precache-manifest.f9ad72c4768bda8cef7ee7e667e300e6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7364a61198ee3357c04feef43fbb144c",
+    "url": "/french-postal/index.html"
+  },
+  {
+    "revision": "a725b84aaa973a344d96",
+    "url": "/french-postal/static/css/main.05145abf.chunk.css"
+  },
+  {
+    "revision": "40d3da5d5b834e8aa68a",
+    "url": "/french-postal/static/js/2.eb3fc7a2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/french-postal/static/js/2.eb3fc7a2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a725b84aaa973a344d96",
+    "url": "/french-postal/static/js/main.b3ed7d60.chunk.js"
+  },
+  {
+    "revision": "a35f02d735dd0901e32e",
+    "url": "/french-postal/static/js/runtime-main.2fd727cb.js"
+  }
+]);
